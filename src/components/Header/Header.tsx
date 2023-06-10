f8894b8c31f11f4ad7192f3c542cf8c9c1696750
Filipe Neves <filipe.nevser@gmail.com>
2023-06-10 18:48:12 -0300

    feat: add header links and dynamic title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,48 @@
+import { useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
+const navLinks = [
+  {
+    label: 'Home',
+    link: '/',
+  },
+  {
+    label: 'Usuários',
+    link: '/users',
+  },
+]
+
 const Header = () => {
-  return <div className="p-5 bg-slate-100 rounded-2xl">Header</div>
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    const currentLink = navLinks.find(({ link }) => link === pathname)
+
+    document.title = `Sparklly Admin${currentLink && `- ${currentLink.label}`}`
+  }, [pathname])
+
+  return (
+    <div className="p-5 bg-slate-100 rounded-2xl flex justify-between">
+      <span>
+        <Link className="text-2xl font-medium" to={'/'}>
+          Sparklly
+        </Link>
+      </span>
+      <div>
+        <nav>
+          <ul className="flex gap-3">
+            {navLinks.map(({ label, link }) => (
+              <li>
+                <Link className="hover:no-underline underline text-xl" to={link}>
+                  {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  )
 }
 
 export default Header
