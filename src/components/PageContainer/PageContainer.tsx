681534eda01f547a5a3ce1c55e40572edabcd0d8
Filipe Neves <filipe.nevser@gmail.com>
2023-06-10 14:54:47 -0300

    feat: add gap space to page container

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -4,7 +4,9 @@ import { PageContainerProps } from './interface'
 const PageContainer: React.FC<PageContainerProps> = ({ children }) => {
   return (
     <div className="h-full w-full flex">
-      <div className="p-5 bg-slate-300 grow rounded-2xl flex flex-col">{children}</div>
+      <div className="p-5 bg-slate-300 grow rounded-2xl flex flex-col gap-5">
+        {children}
+      </div>
     </div>
   )
 }
