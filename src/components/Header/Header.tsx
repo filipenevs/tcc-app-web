5e29481833344e8ef369663c5dce08e95a29b892
Filipe Neves <filipe.nevser@gmail.com>
2023-07-23 00:54:56 -0300

    fix: remove navbar from header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,6 @@
-import { useEffect } from 'react'
-import { Link, useLocation } from 'react-router-dom'
-
-const navLinks = [
-  {
-    label: 'Home',
-    link: '/',
-  },
-  {
-    label: 'Usuários',
-    link: '/users',
-  },
-]
+import { Link } from 'react-router-dom'
 
 const Header = () => {
-  const { pathname } = useLocation()
-
-  useEffect(() => {
-    const currentLink = navLinks.find(({ link }) => link === pathname)
-
-    document.title = `Sparklly Admin${currentLink && `- ${currentLink.label}`}`
-  }, [pathname])
-
   return (
     <div className="p-5 bg-slate-100 rounded-2xl flex justify-between">
       <span>
@@ -28,19 +8,6 @@ const Header = () => {
           Sparklly
         </Link>
       </span>
-      <div>
-        <nav>
-          <ul className="flex gap-3">
-            {navLinks.map(({ label, link }) => (
-              <li key={link}>
-                <Link className="hover:no-underline underline text-xl" to={link}>
-                  {label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
     </div>
   )
 }
