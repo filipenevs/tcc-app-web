080ffafddb8f4d08c56a29d3065508f4c7e5a41f
Filipe Neves <filipe.nevser@gmail.com>
2023-07-25 21:30:46 -0300

    feat: add selection state in locations reducer

--- src/store/reducers/locations.ts
+++ src/store/reducers/locations.ts
@@ -4,11 +4,21 @@ import { State } from '../../typings/address'
 interface AddressState {
   data: State[] | null
   loading: boolean
+  selection: {
+    state: string | null
+    city: string | null
+    neighborhood: string | null
+  }
 }
 
 const initialState: AddressState = {
   data: null,
   loading: false,
+  selection: {
+    state: null,
+    city: null,
+    neighborhood: null,
+  },
 }
 
 const locationsSlide = createSlice({
@@ -29,8 +39,38 @@ const locationsSlide = createSlice({
         data: action.payload,
       }
     },
+    selectState(state, action: PayloadAction<string>) {
+      return {
+        ...state,
+        selection: {
+          state: action.payload,
+          city: null,
+          neighborhood: null,
+        },
+      }
+    },
+    selectCity(state, action: PayloadAction<string>) {
+      return {
+        ...state,
+        selection: {
+          ...state.selection,
+          city: action.payload,
+          neighborhood: null,
+        },
+      }
+    },
+    selectNeighborhood(state, action: PayloadAction<string>) {
+      return {
+        ...state,
+        selection: {
+          ...state.selection,
+          neighborhood: action.payload,
+        },
+      }
+    },
   },
 })
 
-export const { insertStatesData } = locationsSlide.actions
+export const { insertStatesData, selectState, selectCity, selectNeighborhood } =
+  locationsSlide.actions
 export default locationsSlide.reducer
