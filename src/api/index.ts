bc8e442f6f59f007491a90f236602fcec8b8f244
Filipe Neves <filipe.nevser@gmail.com>
2023-07-28 21:27:49 -0300

    feat: add api response interceptors

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,35 @@
-import axios from 'axios'
+import axios, { AxiosResponse, AxiosError } from 'axios'
+
+interface RequisitionError {
+  status?: number
+  message: string
+}
 
 const HOST = 'http://127.0.0.1'
+const PORT = 3333
 
-export const mainApi = axios.create({
-  baseURL: `${HOST}:3332`,
+const mainApi = axios.create({
+  baseURL: `${HOST}:${PORT}`,
 })
+
+mainApi.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  ({ response, message }: AxiosError) => {
+    let error: RequisitionError
+
+    if (!response) {
+      error = {
+        message,
+      }
+    } else {
+      error = {
+        message: (response.data as any).message,
+        status: response.status,
+      }
+    }
+
+    return Promise.reject(error)
+  },
+)
+
+export { mainApi }
