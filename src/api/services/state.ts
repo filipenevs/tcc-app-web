e225766c31060fafa3f131fc41556c0e5c036025
Filipe Neves <filipe.nevser@gmail.com>
2023-07-28 21:28:56 -0300

    fix: add state service instead address service

---
+++ src/api/services/state.ts
@@ -0,0 +1,25 @@
+import { mainApi } from '..'
+
+class StateService {
+  static baseUrl = '/states'
+
+  static async getAllStates() {
+    try {
+      const response = await mainApi.get(`${this.baseUrl}/`)
+      return response.data
+    } catch (error) {
+      throw error
+    }
+  }
+
+  static async deleteState(stateId: string) {
+    try {
+      const response = await mainApi.delete(`${this.baseUrl}/${stateId}`)
+      return response.data
+    } catch (error) {
+      throw error
+    }
+  }
+}
+
+export default StateService
