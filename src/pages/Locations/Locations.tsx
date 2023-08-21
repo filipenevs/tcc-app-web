70d45d46529f98684ed1fa2ac9710c331a539fcb
Filipe Neves <filipe.nevser@gmail.com>
2023-08-21 17:19:51 -0300

    feat: add create state button to locations page

--- src/pages/Locations/Locations.tsx
+++ src/pages/Locations/Locations.tsx
@@ -1,11 +1,34 @@
+import { useState } from 'react'
+
 import PageTitle from '../../components/PageTitle/PageTitle'
 import LocationsList from '../../components/LocationsList/LocationsList'
+import StateForm from '../../components/StateForm/StateForm'
 
 const Locations = () => {
+  const [isAddStateOpen, setIsAddStateOpen] = useState(false)
+
+  function handleOnClickAddState() {
+    setIsAddStateOpen(true)
+  }
+
   return (
     <>
+      {isAddStateOpen && (
+        <StateForm
+          isEdit={false}
+          closeFunction={() => setIsAddStateOpen(false)}
+        />
+      )}
       <div className="flex justify-between">
         <PageTitle text="Localidades" />
+        <div className="flex gap-2">
+          <button
+            className="rounded-md bg-slate-500 hover:bg-slate-600 py-2 px-5 font-medium text-white"
+            onClick={handleOnClickAddState}
+          >
+            Adicionar Estado
+          </button>
+        </div>
       </div>
       <LocationsList />
     </>
